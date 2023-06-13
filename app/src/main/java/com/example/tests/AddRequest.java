ae70aa8f1cdbf4417d982e1b615c812d22dcb458
Ayoub Bahammou <ayoubhisoka01@gmail.com>
2023-06-13 09:02:32 +0200

    new Screens

--- app/src/main/java/com/example/tests/AddRequest.java
+++ app/src/main/java/com/example/tests/AddRequest.java
@@ -17,41 +17,42 @@ public class AddRequest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-        Intent intent2 = new Intent(this, Profile.class);
-        Intent intent3 = new Intent(this, Profile.class);
+        Intent intent1 = new Intent(this, AllRequests.class);
+        Intent intent2 = new Intent(this, MyRequests.class);
+        Intent intent3 = new Intent(this, Chat.class);
         Intent intent4 = new Intent(this, Profile.class);
-        Intent intent5 = new Intent(this, Profile.class);
+        //Intent intent5 = new Intent(this, Profile.class);
 
         binding = AddrequestBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
 
         binding.buttonAll2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                startActivity(intent2);
+                startActivity(intent1);
             }
         });
         binding.buttonMyrequests2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                startActivity(intent3);
+                startActivity(intent2);
             }
         });
         binding.buttonChat2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                startActivity(intent4);
+                startActivity(intent3);
             }
         });
         binding.buttonProfile2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                startActivity(intent5);
+                startActivity(intent4);
             }
         });
     }
